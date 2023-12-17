1026dce44965e37b86ccd7a8ca00ea20940ae8bf
Daniel Turunen <89087279+DanielTurunen2021@users.noreply.github.com>
2023-12-17 19:36:36 +0100

    Added check for touching only once
    Added check for touching only once

--- Assets/Scripts/SpawnObjectOnPlane.cs
+++ Assets/Scripts/SpawnObjectOnPlane.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -8,9 +9,16 @@ public class SpawnObjectOnPlane : MonoBehaviour
 {
     private ARRaycastManager _raycastManager;
     private GameObject _spawnedGameObject;
+    private uint ChestCount = 0;
+    [SerializeField] private uint maxObjects = 0;
+    private uint objectCount = 0;
+    
 
     [SerializeField] 
     private GameObject placeableGameObject;
+
+    [SerializeField] 
+    private GameObject chestToSpawn;
 
     //List of raycast hits
     private static List<ARRaycastHit> _raycastHits = new List<ARRaycastHit>();
@@ -18,17 +26,16 @@ public class SpawnObjectOnPlane : MonoBehaviour
     //list of game objects to track
     private List<GameObject> _trackedObjects = new List<GameObject>();
 
+    [SerializeField] private Animator anim;
+
     private void Awake()
     {
         _raycastManager = GetComponent<ARRaycastManager>();
+        anim = chestToSpawn.GetComponent<Animator>();
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
@@ -44,25 +51,39 @@ public class SpawnObjectOnPlane : MonoBehaviour
             //if we touched the screen. store the first position.
             var hitpos = _raycastHits[0].pose;
 
-            //If we don't already have an object at this location.
+          
             //Spawn an object at this location & add it to the list of tracked objects if the number of objets are less than 3.
-            if (_spawnedGameObject == null && _trackedObjects.Count < 3)
+            if (objectCount < maxObjects)
             {
+                /*
                 _spawnedGameObject = Instantiate(placeableGameObject, hitpos.position, hitpos.rotation);
                 _trackedObjects.Add(_spawnedGameObject);
+                objectCount++;
+                */
+                
+                SpawnPrefab(hitpos);
             }
-            else
+            
+            if (objectCount == maxObjects)
             {
-                _spawnedGameObject.transform.position = hitpos.position;
-                _spawnedGameObject.transform.rotation = hitpos.rotation;
+                if (ChestCount < 1)
+                {
+                    _spawnedGameObject = Instantiate(chestToSpawn, hitpos.position, hitpos.rotation);
+                    anim.Play("Chest_Lid_anim");
+                    ChestCount++;
+                }
+                
             }
+            
         }
     }
+    
 
     //Function for checking if touched the screen. If the touchcount is higher than 0 we store the position of the first touch in the list
     bool TryGetTouchPosition(out Vector2 touchposition)
     {
-        if (Input.touchCount > 0)
+        //Checks if we started touching the screen (so that we only ever place one object every touch)
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
         {
             touchposition = Input.GetTouch(0).position;
             return true;
@@ -70,5 +91,12 @@ public class SpawnObjectOnPlane : MonoBehaviour
 
         touchposition = default;
         return false;
+    }
+
+    void SpawnPrefab(Pose hitpose)
+    {
+        _spawnedGameObject = Instantiate(placeableGameObject, hitpose.position, hitpose.rotation);
+        _trackedObjects.Add(_spawnedGameObject);
+        objectCount++;
     }
 }
